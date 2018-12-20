9501d1cdabd01a6a6b07f04a601d47d4672afe15
Quentin Leffray <leffray.quentin@bcg.com>
2018-12-20 18:25:39 +0100

    working on d8

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_to_string;
+use std::io::Result;
+use std::path::Path;
+
+extern crate regex;
+
+use regex::Regex;
+
+struct Node {
+    children: u32,
+    metadata: u32,
+    entries: Vec<u32>,
+}
+
+fn parse(s: String) -> Vec<u32> {
+    s.split(' ')
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<u32>>()
+}
+
+fn follow(input: &Vec<u32>) -> Vec<Node> {
+    Vec::new()
+}
+
+fn main() -> Result<()> {
+    let s = read_to_string(Path::new("data/input2"))?
+        .trim_end()
+        .to_string();
+    let input = parse(s);
+
+    println!("{:?}", input);
+    println!("sum = {}", input.iter().sum::<u32>());
+
+    Ok(())
 }
