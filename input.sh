64ba2602db4ab9d2266cc3733fbdb5508c91da57
Fiahil <fiahil@gmail.com>
2019-01-05 23:07:22 +0100

    updating input

--- input.sh
+++ input.sh
@@ -2,8 +2,8 @@
 
 for i in {1..25}
 do
-    mkdir -p d$i
+    mkdir -p d$i/data
     curl -s "https://adventofcode.com/2018/day/$i/input" \
-         -H 'cookie: session=53616c7465645f5fefb70adacd3742f25318b7323f7e569c805864464e48a1f9804bd045388846240ed13b406229f497' > d$i/input
+         -H 'cookie:session=53616c7465645f5fefb70adacd3742f25318b7323f7e569c805864464e48a1f9804bd045388846240ed13b406229f497' > d$i/data/input
     echo "D$i [OK]"
 done
