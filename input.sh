8f5e309a2c37487b68c0158c685203e8f0d3a091
Quentin Leffray <leffray.quentin@bcg.com>
2019-11-21 15:25:26 +0100

    moving things around for new edition

--- input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..25}
-do
-    mkdir -p d$i/data
-    curl -s "https://adventofcode.com/2018/day/$i/input" \
-         -H 'cookie:session=53616c7465645f5fefb70adacd3742f25318b7323f7e569c805864464e48a1f9804bd045388846240ed13b406229f497' > d$i/data/input
-    echo "D$i [OK]"
-done
