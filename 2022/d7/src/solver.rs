75361a9ddd5adfdd4aed04133b412d55ff666d40
Quentin Leffray <leffray.quentin@bcg.com>
2023-01-10 18:38:54 +0100

    add d7 & d16

---
+++ 2022/d7/src/solver.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+
+pub fn part1(input: &String) -> Result<i32> {
+    for line in input.lines() {
+        println!("{}", line);
+
+        if line.starts_with("$") {
+            println!("Found a dollar sign");
+        }
+    }
+    Ok(0)
+}
+
+pub fn part2(input: &String) -> Result<i32> {
+    Ok(0)
+}
+
+pub mod test {
+    use super::*;
+
+    pub fn part1(input: &String) -> Result<()> {
+        assert_eq!(super::part1(input)?, 1);
+
+        Ok(())
+    }
+
+    pub fn part2(input: &String) -> Result<()> {
+        assert_eq!(super::part2(input)?, 1);
+
+        Ok(())
+    }
+}
